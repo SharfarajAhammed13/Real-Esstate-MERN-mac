c26d6afed3ab26b1e21a9ef6d2486df455ca4280
SHAIK SHARFARAJ AHAMMED <sharfaraj13@gmail.com>
2023-12-28 20:51:24 +0530

    added functionality for signup UI

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import exp from 'constants';
@@ -15,14 +16,16 @@ mongoose.connect(process.env.MONGO).then (()=> {
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
+
 
 app.listen(3000, () => {
     console.log("Server is Running on 3000!!");
 });
 
-app.use("/api/user",userRouter);
-app.use("/api/auth", authRouter);
+app.use('/api/user',userRouter);
+app.use('/api/auth', authRouter);
 
 
 app.use((err, req, res, next) => {
@@ -33,4 +36,4 @@ app.use((err, req, res, next) => {
         statusCode,
         message,
     });
-})
+});
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,22 +1,9 @@
-import {Link } from "react-router-dom";
 import React from 'react'
 
-export default function SignUp() {
+export default function SignIn() {
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4'> 
-        <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username'></input>
-        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='username'></input>
-        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='username'></input>
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'></button>
-      </form>
-      <div className=''>
-        <p>Have an Account?</p>
-        <Link to="{/sign-in}">
-          <span className="text-blue-700">Sign In</span>
-        </Link>
-      </div>
+    <div>
+      SIGN IN
     </div>
   )
 }
